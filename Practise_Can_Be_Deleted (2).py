27f5215107911402bd77a8a5f3b222b7dae83375
dgthomas <dgthomas@iastate.edu>
2020-12-27 18:50:11 -0600

    Second correct commit

---
+++ Practise_Can_Be_Deleted (2).py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[2]:
+
+
+print("Hell eh World")
+
+
+# In[3]:
+
+
+print("This is going great")
+
+
+# In[ ]:
+
+
+
+
